490bf71caabf82fcdb40d47883bcd6ba40c5103f
Asya Roikhman <roichm@gmail.com>
2022-07-04 22:02:27 +0300

    ObjectsHomework04072022

---
+++ script.js
@@ -0,0 +1,117 @@
+console.log(
+  {
+    shopType: "superMarket",
+    departments: {
+      diary: [
+        {
+          name: "milk",
+          type: "cow",
+          serialNumber: "12345678",
+          price: 5.8,
+          weight: 1000,
+          isKosher: true,
+        },
+        {
+          name: "milk",
+          type: "soy",
+          serialNumber: "1234563",
+          price: 12,
+          weight: 800,
+          isKosher: true,
+        },
+        {
+          name: "cheese",
+          type: "kashkabal",
+          serialNumber: "456237952",
+          price: 22,
+          weight: 200,
+          isKosher: true,
+        },
+      ],
+      meat: [
+        {
+          name: "ribs",
+          type: "lamb",
+          serialNumber: "4568432545",
+          price: 180,
+          weight: 1000,
+          isKosher: true,
+        },
+        {
+          name: "sinta",
+          type: "beef",
+          serialNumber: "568478952",
+          price: 160,
+          weight: 1000,
+          isKosher: true,
+        },
+      ],
+      electronics: [
+        {
+          name: "tV",
+          type: "smart",
+          serialNumber: "22555635",
+          price: 6500,
+          isKosher: null,
+        },
+      ],
+    },
+  },
+  {
+    shopType: "closes",
+    departments: {
+      kids: {
+        boys: [
+          {
+            name: "tShirt",
+            color: "red",
+            serialNumber: "2255337",
+            price: 30,
+            season: "summer",
+          },
+          {
+            name: "pants",
+            color: "blue",
+            serialNumber: "5555787878",
+            print: 20,
+            season: "winter",
+          },
+        ],
+        girls: [
+          {
+            name: "tShirt",
+            color: "white",
+            serialNumber: "2995337",
+            price: 30,
+            season: "summer",
+          },
+          {
+            name: "taits",
+            color: "pink",
+            serialNumber: "2995337",
+            price: 20,
+            season: "summer",
+          },
+        ],
+      },
+      men: [
+        {
+          name: "tShirt",
+          color: "ficsia",
+          serialNumber: "7854542354",
+          price: 200,
+          season: "summer",
+        },
+      ],
+      women: [
+        {
+          name: "tShirt",
+          color: "blue",
+          serialNumber: "45578456",
+          price: 100,
+          season: "summer",
+        },
+      ],
+    },
+  }
+);
